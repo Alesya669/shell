43c63f6f34fa738286b5482a3efa13f2aec92f0c
Alesya <rudenko.alesya2006@mail.ru>
2025-11-22 10:41:58 +0300

    7

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <sstream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -12,6 +14,7 @@ int main()
     bool running = true;
     string history_file = "kubsh_history.txt";
     ofstream write_file(history_file, ios::app);
+    
     while (running && getline(cin, input)) 
     {
         if (input.empty()) {
@@ -19,6 +22,7 @@ int main()
         }
         write_file << input << endl;
         write_file.flush();
+        
         if (input == "\\q") 
         {
             running = false;
@@ -41,49 +45,33 @@ int main()
             cout << text << endl;
             history.push_back(input);
         }
-else if (input.substr(0,4) == "\\e $") {
-
-
-
+        else if (input.substr(0,4) == "\\e $") 
+        {
             string var_name = input.substr(4);
-
-
             const char* env_value = getenv(var_name.c_str());
-
-
-          
+            
             if (env_value != nullptr) {
                 string value(env_value);
                 if (value.find(':') != string::npos) {
                     stringstream ss(value);
                     string item;
                     while (getline(ss, item, ':')) {
-
                         cout << item << endl;
-
                     }
-
                 } else {
-
                     cout << value << endl;
                 }
-
             } else {
-
                 cout << "Environment variable '" << var_name << "' not found" << endl;
-
             }
-
+        }
         else 
         {
-            
             cout << input << ": command not found" << endl;
             history.push_back(input);
         }
-     write_file.close();
-
     }
     
+    write_file.close();
     return 0;
 }
-   
